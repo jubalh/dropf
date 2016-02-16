90a6c5d90205c3dadac293346292fa7cdc6faede
Michael Vetter <jubalh@openmailbox.org>
2016-02-16 18:17:00 +0100

    Split files and add BoltDb functions

--- dropf.go
+++ dropf.go
@@ -1,83 +1,25 @@
 package main
 
 import (
-	"fmt"
-	"html/template"
-	"io"
 	"log"
 	"net/http"
-	"os"
+
+	"github.com/boltdb/bolt"
 )
 
-func executeTemplate(templateName string, writer http.ResponseWriter) {
-	t, err := template.ParseFiles("templates/" + templateName)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-	}
-
-	t.Execute(writer, nil)
-}
-
-// indexHandler serves the index Page where a user can login.
-func indexHandler(response http.ResponseWriter, request *http.Request) {
-	executeTemplate("index.html", response)
-}
-
-// loginHandler is used to log the user in.
-func loginHandler(response http.ResponseWriter, request *http.Request) {
-	name := request.FormValue("name")
-	password := request.FormValue("password")
-	target := "/"
-	if name == "gandalf" && password == "mellon" {
-		target = "/userspace"
-	}
-	http.Redirect(response, request, target, 302)
-}
-
-// userspaceHandler shows the users private home area.
-func userspaceHandler(response http.ResponseWriter, request *http.Request) {
-	executeTemplate("userspace.html", response)
-}
-
-// uploadHandler uploads the file.
-func uploadHandler(response http.ResponseWriter, request *http.Request) {
-	if request.Method == "POST" {
-		file, header, err := request.FormFile("file")
-		if err != nil {
-			fmt.Fprintln(response, "Something went wrong!")
-			fmt.Fprintln(os.Stderr, err)
-			return
-		}
-		defer file.Close()
-
-		os.Mkdir("files", 0660)
-		output, err := os.Create("files/" + header.Filename) //TODO: read config where to save
-		if err != nil {
-			fmt.Fprintln(response, "Something went wrong!")
-			fmt.Fprintln(os.Stderr, err)
-			return
-		}
-		defer output.Close()
-
-		_, err = io.Copy(output, file)
-		if err != nil {
-			fmt.Fprintln(response, err)
-			fmt.Fprintln(os.Stderr, err)
-		}
-
-		fmt.Fprintf(response, "File uploaded successfully: ")
-		fmt.Fprintf(response, header.Filename)
-		fmt.Println("Uploaded file:", header.Filename)
-	}
-}
-
 func main() {
+	db, err := bolt.Open("files.db", 0644, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/login", loginHandler)
 	http.HandleFunc("/userspace", userspaceHandler)
 	http.HandleFunc("/upload", uploadHandler)
 
-	err := http.ListenAndServe(":9090", nil)
+	err = http.ListenAndServe(":9090", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
---
+++ handlers.go
@@ -0,0 +1,74 @@
+// handlers.go contains all the HTTP handlers for dropf
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"io"
+	"net/http"
+	"os"
+)
+
+// executeTemplate loads a basic HTML file and writes it to a writer.
+// Which usually should be a http.ResponseWriter.
+func executeTemplate(templateName string, writer http.ResponseWriter) {
+	t, err := template.ParseFiles("templates/" + templateName)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+
+	t.Execute(writer, nil)
+}
+
+// indexHandler serves the index Page where a user can login.
+func indexHandler(response http.ResponseWriter, request *http.Request) {
+	executeTemplate("index.html", response)
+}
+
+// loginHandler is used to log the user in.
+func loginHandler(response http.ResponseWriter, request *http.Request) {
+	name := request.FormValue("name")
+	password := request.FormValue("password")
+	target := "/"
+	if name == "gandalf" && password == "mellon" {
+		target = "/userspace"
+	}
+	http.Redirect(response, request, target, 302)
+}
+
+// userspaceHandler shows the users private home area.
+func userspaceHandler(response http.ResponseWriter, request *http.Request) {
+	executeTemplate("userspace.html", response)
+}
+
+// uploadHandler uploads the file.
+func uploadHandler(response http.ResponseWriter, request *http.Request) {
+	if request.Method == "POST" {
+		file, header, err := request.FormFile("file")
+		if err != nil {
+			fmt.Fprintln(response, "Something went wrong!")
+			fmt.Fprintln(os.Stderr, err)
+			return
+		}
+		defer file.Close()
+
+		os.Mkdir("files", 0660)
+		output, err := os.Create("files/" + header.Filename) //TODO: read config where to save
+		if err != nil {
+			fmt.Fprintln(response, "Something went wrong!")
+			fmt.Fprintln(os.Stderr, err)
+			return
+		}
+		defer output.Close()
+
+		_, err = io.Copy(output, file)
+		if err != nil {
+			fmt.Fprintln(response, err)
+			fmt.Fprintln(os.Stderr, err)
+		}
+
+		fmt.Fprintf(response, "File uploaded successfully: ")
+		fmt.Fprintf(response, header.Filename)
+		fmt.Println("Uploaded file:", header.Filename)
+	}
+}
